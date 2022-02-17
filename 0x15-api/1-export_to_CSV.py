7b0067d71cbeb7f8343a5320837fc588c39497da
mayii527 <3434@holbertonschool.com>
2022-02-17 14:51:50 -0500

    fix identation

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Using what you did in the task #0,
-extend your Python script to 
+extend your Python script to
 export data in the CSV format.
 """
 import csv
@@ -18,7 +18,7 @@ if __name__ == "__main__":
             dict_user = requests.get(url_user + id_user).json()
             nameEmployee = dict_user.get('username')
             dict_todos_users = requests.get(url_todos_users + id_user).json()
-            
+
             data_user = []
             for i in dict_todos_users:
                 print('hello')
@@ -26,10 +26,11 @@ if __name__ == "__main__":
 
             print_data = []
             for a in data_user:
-                data_user.append(id_user, nameEmployee, a['completed'], a['title'])
+                data_user.append(id_user, nameEmployee,
+                                 a['completed'], a['title'])
 
             with open('file.csv'.format(id_user), 'w', newline='') as f:
-                writer = csv.writer(f, quoting=csv.QUOTE_ALL,delimiter=',')
+                writer = csv.writer(f, quoting=csv.QUOTE_ALL, delimiter=',')
                 writer.writerows(print_data)
     except:
         print('Error')
